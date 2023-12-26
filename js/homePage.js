00108f9be847807042b26bcff9f685f7e239ffc1
Javier Ruedas <jruedas@twc.com>
2023-12-26 10:12:55 -0600

    revise content for information projects

---
+++ js/homePage.js
@@ -0,0 +1,5 @@
+const infoSciTopic = document.querySelector(".infosci");
+
+const goToInfoSciPage = event => location.href = "info-studies.html";
+
+infoSciTopic.addEventListener('click', goToInfoSciPage);
--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 const logo = document.querySelector(".logo");
-const infoSciTopic = document.querySelector(".infosci");
 
 const goToHomePage = event => location.href = "index.html";
-const goToInfoSciPage = event => location.href = "info-studies.html";
 
 logo.addEventListener('click', goToHomePage);
-infoSciTopic.addEventListener('click', goToInfoSciPage);
+
 
 
 

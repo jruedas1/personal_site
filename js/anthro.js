d795101870a471fd3127aa14d881286067fcda1f
Javier Ruedas <jruedas@twc.com>
2024-01-15 11:32:12 -0600

    comment out anthro papers and publications pages and add images to topic divs

--- js/anthro.js
+++ js/anthro.js
@@ -1,11 +1,11 @@
-const publicationsTopic = document.querySelector(".publications");
-const papersTopic = document.querySelector(".papers");
+// const publicationsTopic = document.querySelector(".publications");
+// const papersTopic = document.querySelector(".papers");
 const projectsTopic = document.querySelector(".anthro-projects");
 
 const goToPublications = event => location.href = "anthro-publications.html";
 const goToPapers = event => location.href = "anthro-papers.html";
 const goToProjects = event => location.href = "anthro-projects.html";
 
-publicationsTopic.addEventListener('click', goToPublications);
-papersTopic.addEventListener('click', goToPapers);
+// publicationsTopic.addEventListener('click', goToPublications);
+// papersTopic.addEventListener('click', goToPapers);
 projectsTopic.addEventListener('click', goToProjects);

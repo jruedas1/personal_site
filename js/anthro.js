ff746e1142ddf6d7f1b0cbceb75387a5419b830a
Javier Ruedas <jruedas@twc.com>
2024-01-23 11:42:26 -0600

    add images on anthro page

--- js/anthro.js
+++ js/anthro.js
@@ -1,5 +1,5 @@
 const publicationsTopic = document.querySelector(".publications");
-// const papersTopic = document.querySelector(".papers");
+const papersTopic = document.querySelector(".papers");
 const projectsTopic = document.querySelector(".anthro-projects");
 
 const goToPublications = event => location.href = "anthro-publications.html";
@@ -7,5 +7,5 @@ const goToPapers = event => location.href = "anthro-papers.html";
 const goToProjects = event => location.href = "anthro-projects.html";
 
 publicationsTopic.addEventListener('click', goToPublications);
-// papersTopic.addEventListener('click', goToPapers);
+papersTopic.addEventListener('click', goToPapers);
 projectsTopic.addEventListener('click', goToProjects);
